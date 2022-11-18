c82cde2a3beede8bc30890625a2efab1272c08f5
Emily Hill <emhillmed2021@gmail.com>
2022-11-18 16:12:27 -0600

    figured out how to use periodic-table and node-periodic-table npm packages

---
+++ periodictable.js
@@ -0,0 +1,9 @@
+
+const npt = require('node-periodic-table'); 
+
+console.log(npt.getByName('Hydrogen')); 
+
+const pt= require('periodic-table');
+
+var hydrogen = pt.elements.Hydrogen; 
+console.log(hydrogen)
